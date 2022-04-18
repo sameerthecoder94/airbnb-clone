4daf24500474f805c3bd0f2a5794dd3be61a0db7
sameerthecoder94 <sameerthecoder@gmail.com>
2022-04-18 12:03:51 -0400

    redux added

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
 import App from './App';
 import './index.css';
+
+const rootReducer = combineReducers({
+  app: function () {
+    return {};
+  },
+});
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
